a0d9b366028cc34081396f8237fba4066dd97416
Codaishin <Codaishin@gmail.com>
2020-11-02 13:44:06 +0100

    CustomProject: Fixed back to base functionality

--- CustomProject/Program.cs
+++ CustomProject/Program.cs
@@ -38,23 +38,10 @@ namespace CustomProject
 		{
 			Player player = new Player { damage = 10 };
 			Monster monster = new Monster { hp = 40 };
-			// while (monster.hp > 0) {
-			// 	Program.Render(player, monster);
-			// 	Program.PlayerAttack(player, monster);
-			// }
-
-			Dictionary<PlayerType, int> dict = new Dictionary<PlayerType, int> {
-				{ PlayerType.Fighter, 40 },
-				{ PlayerType.Mage, 6 },
-			};
-
-			foreach (KeyValuePair<PlayerType, int> kvp in dict) {
-				Console.WriteLine(((int)kvp.Key + 1, kvp.Key, kvp.Value));
-			}
-			string input = Console.ReadLine();
-			int.TryParse(input, out int parsed);
-			PlayerType selected = (PlayerType)(parsed - 1);
-			Console.WriteLine(("selected", selected, dict[selected]));
+			 while (monster.hp > 0) {
+			 	Program.Render(player, monster);
+			 	Program.PlayerAttack(player, monster);
+			 }
 		}
 	}
 }

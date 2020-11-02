08c817d360b47779825425b5d56165b80c421dfc
Codaishin <Codaishin@gmail.com>
2020-11-02 14:49:09 +0100

    CustomProject: Implement Accuracy and Evasion

--- CustomProject/GameObject.cs
+++ CustomProject/GameObject.cs
@@ -4,14 +4,31 @@ namespace CustomProject
 {
 	struct GameObject
 	{
+		private static Random rand = new Random();
+
+		public string name;
+
 		public int hp;
 		public int damage;
-		public string name;
+		public int evasion;
+		public int accuracy;
 
 		public static void Init(out GameObject player, out GameObject monster)
 		{
-			player = new GameObject { name = "player", hp = 50, damage = 10 };
-			monster = new GameObject { name = "monster", hp = 40, damage = 5 };
+			player = new GameObject {
+				name = "player",
+				hp = 50,
+				damage = 10,
+				accuracy = 50,
+				evasion = 80,
+			};
+			monster = new GameObject {
+				name = "monster",
+				hp = 40,
+				damage = 5,
+				accuracy = 100,
+				evasion = 10,
+			};
 		}
 
 		public static void Render(params GameObject[] objects)
@@ -24,10 +41,19 @@ namespace CustomProject
 
 		public static void Attack(in GameObject attkr, ref GameObject trgt)
 		{
-			Console.WriteLine(
-				$"-> {attkr.name}: attacks {trgt.name} for {attkr.damage} damage"
-			);
-			trgt.hp -= attkr.damage;
+			Console.WriteLine($"-> {attkr.name}: attacks {trgt.name}");
+			if (GameObject.Hit(attkr, trgt)) {
+				trgt.hp -= attkr.damage;
+				Console.WriteLine($"-> {attkr.damage} damage");
+			} else {
+				Console.WriteLine("-> MISS!");
+			}
+		}
+
+		private static bool Hit(in GameObject attkr, in GameObject trgt)
+		{
+			int hitMax = attkr.accuracy + trgt.evasion + 1;
+			return GameObject.rand.Next(0, hitMax) > trgt.evasion;
 		}
 	}
 }

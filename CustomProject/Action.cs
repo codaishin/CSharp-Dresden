163e61627ada87219472c9432419742cacd86eff
Codaishin <Codaishin@gmail.com>
2020-11-02 16:15:14 +0100

    CustomProject: Refactored Action.cs to utilize a Dictionary

--- CustomProject/Action.cs
+++ CustomProject/Action.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace CustomProject
 {
@@ -11,30 +12,23 @@ namespace CustomProject
 
 	static class Action
 	{
-		static Choice[] choices = new Choice[] {
-			new Choice{ selector = "n", action = ConcreteAction.Nothing },
-			new Choice{ selector = "a", action = ConcreteAction.Attack },
-			new Choice{ selector = "d", action = ConcreteAction.Dodge },
-		};
-
-		struct Choice
-		{
-			public string selector;
-			public ConcreteAction action;
-
-			public override string ToString() => $"{this.selector}: {this.action}";
-		}
+		static Dictionary<string, ConcreteAction> choices
+			= new Dictionary<string, ConcreteAction>() {
+				{ "n", ConcreteAction.Nothing },
+				{ "a", ConcreteAction.Attack },
+				{ "d", ConcreteAction.Dodge },
+			};
 
 		public static ConcreteAction SelectAction()
 		{
 			Console.WriteLine("What do you want to do?");
 			Console.WriteLine(string.Join(", ", Action.choices));
-			switch (Console.ReadLine()) {
-				case "a": return ConcreteAction.Attack;
-				case "d": return ConcreteAction.Dodge;
-				case "n":
-				default: return ConcreteAction.Nothing;
+
+			string input = Console.ReadLine();
+			if (Action.choices.TryGetValue(input, out ConcreteAction action)) {
+				return action;
 			}
+			return default;
 		}
 	}
 }
